c73b6ac6d178bf772784aa41d6b9b495d6504f5a
Anders Vestergaard <ahv@corduratech.dk>
2021-10-31 12:22:54 +0100

    deleted public

--- public/component---src-pages-404-tsx-ceb303d118f95e5e4bb5.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkay_say=self.webpackChunkay_say||[]).push([[218],{938:function(n,e,t){t.r(e);var u=t(1721),r=t(7294),a=function(n){function e(){return n.apply(this,arguments)||this}return(0,u.Z)(e,n),e.prototype.render=function(){return r.createElement("div",null,"404 - page not found")},e}(r.PureComponent);e.default=a}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-ceb303d118f95e5e4bb5.js.map
